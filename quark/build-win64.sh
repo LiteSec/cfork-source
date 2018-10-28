2c533c8c989d880c4723f842a1366c04a908deab
zer0conf <red@litesec.co>
2018-10-28 16:49:08 +0200

    Update build-win64.sh

--- quark/build-win64.sh
+++ quark/build-win64.sh
@@ -1,3 +1,3 @@
 ./autogen.sh # not required when building from tarball
-CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/
-make
+CONFIG_SITE={}/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/
+make -j2
